be06c35cb323c2ee9a0574ef956ee718b1ffa3d7
dxyrell <lucasadayrell10@gmail.com>
2025-06-10 20:09:46 -0300

    fix: instalando cors e alterando no server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocs = require('./configs/SwaggerConfig');
 
@@ -9,19 +10,25 @@ const reviewRoutes = require('./reviews/routes');
 
 const app = express();
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.use(express.json()); 
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.get('/swagger.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerDocs);
 });
 
-app.use(express.json()); 
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/auth', auth);
 app.use('/user', user);
 app.use('/movies', movie);
 app.use('/reviews', reviewRoutes);
 
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
+    console.log("Servidor rodando na porta 3000");
 });
